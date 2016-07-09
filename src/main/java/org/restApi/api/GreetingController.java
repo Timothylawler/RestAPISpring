8f396ad50d23241082a99e63f5c512e39647c128
timothy.karvonen@hotmail.se <tika1401@student.miun.se>
2016-07-09 21:26:33 +0200

    Added Create, update, delete function to the api

--- src/main/java/org/restApi/api/GreetingController.java
+++ src/main/java/org/restApi/api/GreetingController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,10 +25,30 @@ public class GreetingController {
             greetingMap = new HashMap<BigInteger, Greeting>();
             nextId = BigInteger.ONE;
         }
+        //  if update..
+        if(greeting.getId() != null){
+            Greeting oldGreeting = greetingMap.get(greeting.getId());
+            if(oldGreeting == null)
+                return null;
+            
+            greetingMap.remove(greeting.getId());
+            greetingMap.put(greeting.getId(), greeting);
+            return greeting;
+        }
+        
+        //  If Create..
         greeting.setId(nextId);
         nextId = nextId.add(BigInteger.ONE);
         greetingMap.put(greeting.getId(), greeting);
         return greeting;
+    }
+    
+    private static boolean delete(BigInteger id){
+        Greeting deleteGreeting = greetingMap.remove(id);
+        //  If the greeting was nog found in the map, return false
+        if(deleteGreeting == null)
+            return false;
+        return true;
     }
     
     static {
@@ -40,6 +61,9 @@ public class GreetingController {
         save(g2);
     }
     
+    /**
+     * @return  Returns all greetings available
+     */
     @RequestMapping(
             value="/api/greetings"
             , method=RequestMethod.GET
@@ -52,8 +76,13 @@ public class GreetingController {
             HttpStatus.OK);
     }
     
+    /**
+     * Get a greeting with a specific id
+     *  eg path url "localhost:8080/api/greetings/1"
+     * @param id    entity id
+     */
     @RequestMapping(
-            value="/api/greeting/{id}",
+            value="/api/greetings/{id}",
             method = RequestMethod.GET,
             produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Greeting> getGreeting(@PathVariable("id") BigInteger id){
@@ -65,4 +94,67 @@ public class GreetingController {
         return new ResponseEntity<Greeting>(greeting, HttpStatus.OK);
     }
     
+    
+    /**
+     * Used for creating greeting entities
+     */
+    @RequestMapping(
+            value="/api/greetings",
+            method=RequestMethod.POST,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Greeting> createGreeting(
+            @RequestBody Greeting greeting){
+        
+        //  Save the greeting passed and return it with status.CREATED
+        Greeting savedGreeting = save(greeting);
+        return new ResponseEntity<Greeting>(savedGreeting, HttpStatus.CREATED);
+    }
+    
+    
+    /**
+     * Used for updating an existing entity
+     * @param greeting  Entity to be updated
+     * @return  The updated entity
+     */
+    @RequestMapping(
+            value="/api/greetings/{id}",
+            method=RequestMethod.PUT,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Greeting> updateGreeting(
+            @RequestBody Greeting greeting){
+        
+        //  could extract and check the id from the url but that is juset
+        //      there to how that this is possible. as for now just pass
+        //      the id as json param along with the text
+        
+        Greeting updatedGreeting = save(greeting);
+        //  If the greeting was not found in out list, return internal server error
+        if(updatedGreeting == null)
+            return new ResponseEntity<Greeting>(HttpStatus.INTERNAL_SERVER_ERROR);
+        
+        return new ResponseEntity<Greeting>(updatedGreeting, HttpStatus.OK);
+    }
+    
+    /**
+     * Used for deleteing an entity corresponding to the passed id
+     * @param id        key for the entity to delete
+     * @param greeting  Not supported, could be used if wanting to pass a model
+     * @return          Server error(error) or no content(succesful delete)
+     */
+    @RequestMapping(
+            value="/api/greetings/{id}",
+            method=RequestMethod.DELETE,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Greeting> deleteGreeting(
+            @PathVariable("id") BigInteger id,
+            @RequestBody Greeting greeting){
+        
+        boolean deleted = delete(id);
+        if(!deleted)
+            return new ResponseEntity<Greeting>(HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<Greeting>(HttpStatus.NO_CONTENT);
+    }
 }
